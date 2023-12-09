f82bd04fc44fa9cb3f395648dfa050bf5ef5cf83
mrpradeep1 <112063754+mrpradeep1@users.noreply.github.com>
2023-12-09 16:26:08 +0530

    Update HelloWorld.java for feature subtraction

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
                 int a = 5;
                 int b = 10;
                 int c = a + b;
+                int d = b - a;
                 System.out.println("Sum of two numbers:" + c);
+                System.out.println("Difference of two numbers" + d);
         }
 }
